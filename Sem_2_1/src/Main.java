b62b39c5aa91fb781b46e83295b35b6df6216633
Pavel <pavel@b2profit.ru>
2024-04-04 20:24:25 +0300

    2 семинар 3 задача

--- Sem_2_1/src/Main.java
+++ Sem_2_1/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -14,34 +15,127 @@ public class Main {
 //            System.out.println("Указан элемент за пределами массива " + e.getClass());
 //        } catch (InputMismatchException e){
 //        System.out.println("Индекс должен быть целым числом " + e.getClass());}
-String [][] array = new String[][]
-    }
 
-    /**
-     * Задание №2
-     * 📔 Обработайте возможные исключительные ситуации. “Битые” значения в
-     * исходном массиве считайте нулями. Можно внести в код правки, которые считаете
-     * необходимыми
-     */
+//        String[][] array = {{"1", "2", "3"},{"b", "2", "2"}, {"3", "2n", "1"}};
+//        System.out.println(summ2d(array));
 
-    public static int summ2d(String[][] arr) {
-        int sum = 0;
+        /**
+         * Исправьте код, примените
+         * подходящие способы
+         * обработки исключений.
+
+         public static void main(String[] args) {
+         InputStream inputStream;
+         try {
+         String[] strings = {"asdf", "asdf" };
+         String strings1 = strings[2];
+         test();
+         int a = 1 / 0;
+         inputStream = new FileInputStream("adafqdsaf");
+         } catch (Throwable e) {
+         e.printStackTrace();
+         } catch (StackOverflowError error) {
+         System.out.println("asdfasdfdsa");
+         } finally {
+         System.out.println("Я всё равно выполнился!");
+         }
+         System.out.println("Я жив!");
+         }
+
+         public static void test() throws IOException {
+         File file = new File("1");
+         file.createNewFile();
+         FileReader reader = new FileReader(file);
+         reader.read();
+         test();
+         } */
+        InputStream inputStream;
         try {
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < 5; j++) {
-                    int val = Integer.parseInt(arr[i][j]);
-                    sum += val;
-                }
-
-            }
-        } catch (InputMismatchException e){
-            System.out.println("Один из элементов массива - не целое число");
+            String[] strings = {"asdf", "asdf" };
+            String strings1 = strings[2]; // Выход за пределы массива
+            test(); // Переполнение стека рекурсией
+            int a = 1 / 0; // Деление на ноль
+            inputStream = new FileInputStream("adafqdsaf"); // Ошибка ввода вывода
+            /*
+            Строки выше с разными видами исключений надо поместить в разные блоки try - catch
+             */
         } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Строки массива не должны быть длиннее пяти элементов");
+            System.out.println("Выход за пределы массива");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Я всё равно выполнился!");
         }
-
-        return sum;
-
+        System.out.println("Я жив!");
     }
 
+    public static void test() throws IOException {
+        File file = new File("1");
+        file.createNewFile();
+        FileReader reader = new FileReader(file);
+        reader.read();
+        test();
+    }
 }
+
+/**
+ * Задание №2
+ * 📔 Обработайте возможные исключительные ситуации. “Битые” значения в
+ * исходном массиве считайте нулями. Можно внести в код правки, которые считаете
+ * необходимыми
+ * <p>
+ * public static int summ2d(String[][] arr) {
+ * int sum = 0;
+ * for (int i = 0; i < arr.length; i++) {
+ * for (int j = 0; j < 5; j++) {
+ * int val = Integer.parseInt(arr[i][j]);
+ * sum += val;
+ * }
+ * }
+ * return sum;
+ * }
+ * <p>
+ * Исправьте код, примените
+ * подходящие способы
+ * обработки исключений.
+ * <p>
+ * Исправьте код, примените
+ * подходящие способы
+ * обработки исключений.
+ * <p>
+ * Исправьте код, примените
+ * подходящие способы
+ * обработки исключений.
+ * <p>
+ * Исправьте код, примените
+ * подходящие способы
+ * обработки исключений.
+ */
+
+//    public static int summ2d(String[][] arr) {
+//        int sum = 0;
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                try {
+//                    int val = Integer.parseInt(arr[i][j]);
+//                    sum += val;
+//                } catch (NumberFormatException e) {
+//                    System.out.println("Не удалось перевести элемент из строки в целое число");
+//                    sum += 0;
+//                }
+//            }
+//        }
+//        return sum;
+//    }
+//
+//}
+
+/**
+ * Исправьте код, примените
+ * подходящие способы
+ * обработки исключений.
+ *
+ *
+ */
+
