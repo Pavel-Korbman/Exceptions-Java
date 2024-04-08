b8148d4502969cec1059e43e673146e54d5f4bda
Pavel <pavel@b2profit.ru>
2024-04-08 20:12:27 +0300

    Лекция 3 ч1

--- Lect3/src/Main.java
+++ Lect3/src/Main.java
@@ -1,6 +1,60 @@
+/**
+ * Продвинутая работа с исключениями в Java
+ */
+
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Блок try-with-resources - автоматом закрывает открытые ресурсы
+         *
+         * При использовании внешних для JVM ресурсов, таких как файлы,
+         * сетевые соединения, соединения с базами данных и прочие,
+         * требуется обязательно закрывать их в блоке finally. Это связано с
+         * тем, что если приложение аварийно завершит свою работу
+         */
+
+
+        /**
+         *Чтение файла без try-with-resources
+         */
+//        FileReader test = null;
+//        try {
+//            test = new FileReader(new File("file.txt"));
+//// Полезная работа, связанная с чтением файла.
+//        } catch (RuntimeException | IOException e) {
+//            System.out.println("catch exception: " + e.getClass().getSimpleName());
+//        } finally {
+//            try {
+//                if (test != null) {
+//                    test.close();  // Обязательно закрываем файл
+//                }
+//            } catch(IOException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("finally finished");
+//        }
+
+        /**
+         *Чтение файла с try-with-resources
+         */
+
+        try (BufferedReader test = new BufferedReader(new FileReader("file.txt"))) {
+//            Ресурс в скобках будет закрыт по окончании блока try
+            String str;
+            while ((str = test.readLine()) != null) {
+                System.out.println(str);
+            }
+            // Полезная работа, связанная с чтением файла.
+        } catch (RuntimeException | IOException e) {
+            System.out.println("catch exception: " + e.getClass().getSimpleName());
+        } finally {
+//            Не нужно закрывать открытые ресурсы
+            System.out.println("finally start");
+            System.out.println("finally finished");
+        }
+
 
     }
 }
