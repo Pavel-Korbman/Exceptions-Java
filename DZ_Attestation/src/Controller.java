08f1dadfa04ab2e96adde2e3792da0a0d8ca6375
Pavel <pavel@b2profit.ru>
2024-04-09 04:38:58 +0300

    ДЗ Аттестация Корбман П А 5828

--- DZ_Attestation/src/Controller.java
+++ DZ_Attestation/src/Controller.java
@@ -5,33 +5,21 @@ import java.util.zip.DataFormatException;
 public class Controller {
     public static void validationData(List<String> data) throws DataFormatException {
 
-        if (data.size() < 6) {
-            throw new DataFormatException("Вы ввели меньше данных, чем требуется !");
-        }
-        if (data.size() > 6) {
-
-            throw new DataFormatException("Вы ввели больше данных, чем требуется !");
-        }
+        if (data.size() < 6) throw new DataFormatException("Вы ввели меньше данных, чем требуется !");
+        if (data.size() > 6) throw new DataFormatException("Вы ввели больше данных, чем требуется !");
 
         System.out.println("Ваши данные:");
         if (data.getFirst().length() >= 2) System.out.println(data.getFirst());
         else throw new DataFormatException("Слишком короткая фамилия !");
 
         if (data.get(1).length() >= 2) System.out.println(data.get(1));
-        else {
-            throw new DataFormatException("Слишком короткое имя !");
-
-        }
+        else throw new DataFormatException("Слишком короткое имя !");
 
         if (data.get(2).length() >= 2) System.out.println(data.get(2));
-        else {
-            throw new DataFormatException("Слишком короткое отчество !");
-        }
+        else throw new DataFormatException("Слишком короткое отчество !");
 
         if (data.get(3).length() == 10) System.out.println(data.get(3));
-        else {
-            throw new DataFormatException("Не корректный ввод даты !");
-        }
+        else throw new DataFormatException("Не корректный ввод даты !");
 
         if (data.get(4).length() == 10) {
             try {
@@ -40,16 +28,18 @@ public class Controller {
             } catch (NumberFormatException e) {
                 System.out.println(" Телефон должен состоять только из цифр");
             }
-        } else {
-            throw new DataFormatException("Номер телефона должен состоять из 10 цифр !");
-        }
+        } else throw new DataFormatException("Номер телефона должен состоять из 10 цифр !");
 
         if (data.get(5).equalsIgnoreCase("м") | data.get(5).equalsIgnoreCase("ж")) {
             System.out.println(data.get(5));
         } else throw new DataFormatException("Небинарным личность вход закрыт! Пол только м или ж .");
 
-        if (data.size() == 6 && data.getFirst().length() >= 2 && data.get(1).length() >= 2
-                && data.get(2).length() >= 2 && data.get(3).length() == 10 && data.get(4).length() == 10
+        if (data.size() == 6
+                && data.getFirst().length() >= 2
+                && data.get(1).length() >= 2
+                && data.get(2).length() >= 2
+                && data.get(3).length() == 10
+                && data.get(4).length() == 10
                 && data.get(5).equalsIgnoreCase("м") | data.get(5).equalsIgnoreCase("ж")
                 && new BigInteger(data.get(4)) != null) {
             Data.dataSave(data);
--- DZ_Attestation/src/Main.java
+++ DZ_Attestation/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.zip.DataFormatException;
-
 public class Main {
     public static void main(String[] args) throws DataFormatException {
         View.inputData();
